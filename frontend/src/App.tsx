d8f995b87da434514521fc72ee5be4a82db779e3
Svetlana <svetlana.tsibenova@gmail.com>
2023-01-20 11:05:26 +0300

    created Appbar(navigation)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Auth from './features/auth/Auth';
+import Navigation from './features/Navigation';
 
 function App(): JSX.Element {
   return (
     <div>
-      
+      <Navigation />
+      <Auth />
     </div>
   );
 }
